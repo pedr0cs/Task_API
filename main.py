3d8bf69129dd815caf6d0820da557129acc18116
Pedr0cs <taving04@icloud.com>
2026-07-11 16:27:46 -0500

    Atualizando as tarefas com .put

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI 
 
 app = FastAPI()
-@app.get("/")
-def funcao():
-    return{"mensagem": "API funcionando"}
 
+tarefas = [
+    {"id": 1, "descricao": "Estudar FastAPI", "concluida": False},
+    {"id": 2, "descricao": "Fazer exercícios", "concluida": False}
+]
+
+@app.get("/")
+def root():
+    return {"mensagem": "API de Tarefas funcionando!"}
+
+@app.get("/tarefas")
+def listar_tarefas():
+    return tarefas
+
+@app.put('/tarefas/{task_id}')
+def atualizar_tarefa(task_id: int):
+    for task in tarefas:
+        if task['id'] == task_id:
+            task['concluida'] = True
+            return {'mensagem': f'Tarefa {task_id} Atualizada!'}
