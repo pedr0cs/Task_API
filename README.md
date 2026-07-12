e4a939a0cf87ad975958a2053e217ff9c40befbf
Pedr0cs <taving04@icloud.com>
2026-07-12 17:19:06 -0500

    Finalizado CRUD na API e ajuste no README

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
-##Task API
-API REST para gerenciamento de tarefas, construidas com FASTAPI e Python
+# Task API
+# Descricao
+API REST para gerenciamento de tarefas, construidas com FASTAPI e Python. Um projeto de estudos para aprender desenvolvimento backend.
 
-##Objetivo
-Projeto de estudos para aprender desenvolvimento backend
+# Funcionalidades
+API REST que permite adicionar itens a uma lista de tarefas, atualizar, listar e deletar (CRUD) tarefas do gerenciador.
+
+# Tecnologias
+Python
+FastAPI
+Uvicorn
+Git
+SQLite
+ 
+# Como Rodar:
+1- Clone o repositorio
+git clone https://github.com/pedr0cs/task_api.git
+2- Ative o venv
+.\\venv\\Scripts\\Activate
+3- Instale as dependencias
+pip install -r requirements.txt
+4- Rode o servidor
+uvicorn main:app --reload 
+
+# Documentação
+Acesse http://localhost:8000/docs para ver a documentação interativa
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+from asyncio import Task
+from nt import remove
 from fastapi import FastAPI 
+ 
+
+
 
 app = FastAPI()
 
@@ -21,3 +26,35 @@ def atualizar_tarefa(task_id: int):
         if task['id'] == task_id:
             task['concluida'] = True
             return {'mensagem': f'Tarefa {task_id} Atualizada!'}
+
+@app.post("/tarefas")
+def adicionar (descricao: str):
+    id_maior = 0
+    for task in tarefas:
+       if task['id'] > id_maior:
+            id_maior = task["id"]
+
+    novo_id = id_maior + 1
+
+    tarefa_nova = {
+        "id": novo_id,
+        "descricao": 'Estudar Python',
+        "conluida": False
+    }
+
+    tarefas.append(tarefa_nova)
+    return {"mensagem": "Tarefa adicionada"}
+
+@app.delete("/tarefas/{task_id}")
+def apagar (task_id: int):
+    tarefa_encontrada = None
+    for task in tarefas:
+        if task["id"] == task_id:
+            tarefa_encontrada = task
+
+    if tarefa_encontrada:
+        tarefas.remove(tarefa_encontrada)
+        return{'mensagem': "Tarefa removida"}
+    
+    else:
+        return{"mensagem": "Tarefa nao encontrada, verifique o id"}
